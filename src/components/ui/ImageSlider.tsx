d6e97205491e5aac28195c5a011f09eff83cde9d
akash vishwakarma <118232727+savin8305@users.noreply.github.com>
2024-10-08 11:21:41 +0530

    Update ImageSlider.tsx

--- src/components/ui/ImageSlider.tsx
+++ src/components/ui/ImageSlider.tsx
@@ -1,34 +1,32 @@
-"use client";
-import { useMemo } from "react";
-import Image from "next/image";
+import { useRef } from "react";
 import data from "../Constants/hero.json";
 
 const ImageSlider: React.FC = () => {
-  // Extract the HeroSection data and get the first image
   const homeData = data.find((item) => item.category === "HeroSection")?.data;
-  const images = useMemo(() => homeData?.images || [], [homeData]);
-  const singleImageSrc = images.length > 0 ? images[0] : ""; // Get the first image
+  const videoSources = homeData?.video?.sources || [];
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-  if (!singleImageSrc) {
-    return <p>No images available.</p>; // Fallback if there are no images
+  if (!videoSources.length) {
+    return <p>No video available.</p>;
   }
 
   return (
     <div className="relative w-full mx-auto h-full">
-      <div className="w-full h-full bg-white rounded-3xl bg-center">
-        <Image
-          priority
-          src="https://www.nesscoindia.com/Assets/images/resource/fully-automatic-paper-cup-making-machine.webp"
-          alt="Single Image"
-          width={600}
-          height={400}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 600px"
-          placeholder="blur"
-          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAAAAAAAD/..." // Use your low-res image or environment variable
-          loading="eager"
-          className="w-full h-full object-cover rounded-2xl"
-        />
-      </div>
+      <video
+        ref={videoRef}
+        className="w-full h-full object-cover rounded-2xl"
+        autoPlay={false}
+        loop
+        muted
+        controls={false}
+        playsInline
+        preload="metadata" // Preload only metadata for faster loading
+      >
+        {videoSources.map((source, index) => (
+          <source key={index} src={source.src} type={source.type} />
+        ))}
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
